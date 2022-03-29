1d925bbad7c3e897ab901104f77e9f7f84a0df16
Gustavo Navarro <genr1818@gmail.com>
2022-03-29 13:02:31 -0600

    Peticiones a APIs usando Callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -4,8 +4,8 @@ function fetchData(url, callback) {
     let xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
     xhttp.onreadystatechange = function (event) {
-        if(xhttp.readyState === 4) {
-            if(xhttp.status === 200) {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
                 const error = new Error('Error ' + url);
