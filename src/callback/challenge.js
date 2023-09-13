b0f48f800829b51b10c6605d196180830feb3b7e
Gustavo Navarro <genr1818@gmail.com>
2023-09-13 14:26:34 -0600

    mejorando comentarios

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,9 +1,9 @@
 // importamos el modulo para hacer las peticiones
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 // direccion de la API
-let api = 'https://rickandmortyapi.com/api/character/';
+const API = 'https://rickandmortyapi.com/api/character/';
 
-// funcion principal
+
 function fetchData(url_api, callback) {
   // instanciamos la conexion
   let xhttp = new XMLHttpRequest();
@@ -11,9 +11,9 @@ function fetchData(url_api, callback) {
   xhttp.open('GET', url_api, true);
   // validacion del llamado
   xhttp.onreadystatechange = (event) => {
-    // el state 4 es el ultimo de la peticion
-    if (xhttp.readyState === 4) {
-      // verificamos que el status este en 200, que dice que todo bien, no un 400 o 500
+    // el state 4 es el ultimo de la peticion (done), indica que se ha completado
+    if (xhttp.readyState === xhttp.DONE) {
+      // verificamos que el status sea 200 -> Ok
       if (xhttp.status === 200) {
         // el primer valor es el err, y el siguiente el resultado
         // ejecutamos el callback con el resultado
@@ -31,26 +31,26 @@ function fetchData(url_api, callback) {
 }
 
 // primero buscamos la lista de personajes
-fetchData(api, (error1, data1) => {
+fetchData(API, (error1, data1) => {
   // si error, matamos retornando un error
   if (error1) return console.error(error1);
-  // luego buscamos en la api el id de Rick
-  fetchData(api + data1.results[0].id, (error2, data2) => {
+  // luego buscamos en la API el primer id
+  fetchData(API + data1.results[0].id, (error2, data2) => {
     // si error, matamos retornando un error
     if (error2) return console.error(error2);
-    // por ultimo la consulta a la api que contiene su dimension
+    // por ultimo la consulta a la API que contiene su dimension
     fetchData(data2.origin.url, (error3, data3) => {
       // si error, matamos retornando un error
       if (error3) return console.error(error3);
 
       // mostramos los resultados :) 
-      console.log(data1.info.count);
-      console.log(data2.name);
-      console.log(data3.dimension);
+      console.log(data1.info.count); // Numero de personajes
+      console.log(data2.name); // Nombre del personaje id=1
+      console.log(data3.dimension); // Dimension -> origin.url
 
       // rutas de las peticiones en orden
-      console.log(api);
-      console.log(api + data1.results[0].id);
+      console.log(API);
+      console.log(API + data1.results[0].id);
       console.log(data2.origin.url);
 
     });
